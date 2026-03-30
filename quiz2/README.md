4f1d659c542128bebfddfe6617f444a886022d96
mannhorse <oliverherrick11@gmail.com>
2026-03-30 18:41:38 -0400

    Quiz2

---
+++ quiz2/README.md
@@ -0,0 +1,4 @@
+GitHub ID: mannhorse
+Repo Name: mannhorse/itws1100-herrio
+Azure Homepage Link: http://herriorpi.eastus.cloudapp.azure.com/iit/index.html
+Discord: mannhorse
--- quiz2/quiz2.js
+++ quiz2/quiz2.js
@@ -2,3 +2,30 @@
 // Put your javascript here in a document.ready function
 
 
+
+alert("The page is about to load!");
+
+$(document).ready(function () {
+
+  var defaultTitle = "ITWS 1100 - Quiz 2";
+  var myTitle = "Oliver Herrick - Quiz 2";
+  document.title = defaultTitle;
+
+  $("#gobutton").click(function () {
+    if (document.title === defaultTitle) {
+      document.title = myTitle;
+    } else {
+      document.title = defaultTitle;
+    }
+  });
+
+
+  $("#lastname").on("mouseenter", function () {
+    $(this).addClass("makeItPurple");
+  });
+
+  $("#lastname").on("mouseleave", function () {
+    $(this).removeClass("makeItPurple");
+  });
+
+});
