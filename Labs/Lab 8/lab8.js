c291b94b21f3e95c5fb37eb3ce956f8fb8efc501
mannhorse <oliverherrick11@gmail.com>
2026-04-27 18:52:59 -0400

    lab 10

--- Labs/Lab 8/lab8.js
+++ Labs/Lab 8/lab8.js
@@ -8,10 +8,15 @@ $(document).ready(function() {
          var output = "";
 
          $.each(data.menuItem, function(i, item) {
+            var lockIcon = item.secure ? ' &#x1F512;' : '';
             output += '<article class="lab-card">';
-            output += '<h3>' + item.lab + ': ' + item.title + '</h3>';
+            output += '<h3>' + item.lab + ': ' + item.title + lockIcon + '</h3>';
             output += '<p>' + item.description + '</p>';
-            output += '<a href="' + item.link + '">View ' + item.lab + ' &rarr;</a>';
+            output += '<a href="' + item.link + '"';
+            if (item.secure) {
+               output += ' title="Password required"';
+            }
+            output += '>View ' + item.lab + ' &rarr;</a>';
             output += '</article>';
          });
 
